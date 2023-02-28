5b7f88df0c7883cac4207e94997ecbe6431e44d6
Manos Tzelepis <tzeldev@gmail.com>
2023-02-28 12:57:08 +0200

    testt

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 
 function App() {
-  console.log('woot');
+  console.log('woot2');
   console.log('PUBLIC_URL=', process.env.PUBLIC_URL);
   return (
     <div className="App">
